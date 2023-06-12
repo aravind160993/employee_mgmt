bbd1584a040b396648b02e7bbed1892d12695a09
aravind160993 <aravind@macappstudio.com>
2023-06-12 17:34:54 +0530

    Resetting application

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use Laravel\Passport\Http\Controllers\AccessTokenController;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\DepartmentController;
-use App\Http\Controllers\EmployeeController;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,32 +14,6 @@ use App\Http\Controllers\EmployeeController;
 |
 */
 
-// Issue access tokens
-Route::post('/oauth/token', [AccessTokenController::class, 'issueToken']);
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::middleware('auth:api')->group(function () {
-    // Protected routes
-    Route::get('/detail', [AuthController::class, 'detail']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-
-    // For departments
-    Route::get('/departments', [DepartmentController::class, 'index']);
-    Route::post('/departments', [DepartmentController::class, 'store']);
-    Route::get('/departments/{id}', [DepartmentController::class, 'show']);
-    Route::put('/departments/{id}', [DepartmentController::class, 'update']);
-    Route::delete('/departments/{id}', [DepartmentController::class, 'destroy']);
-
-    // For employees
-    Route::get('/employees', [EmployeeController::class, 'index']);
-    Route::post('/employees', [EmployeeController::class, 'store']);
-    Route::get('/employees/{id}', [EmployeeController::class, 'show']);
-    Route::put('/employees/{id}', [EmployeeController::class, 'update']);
-    Route::delete('/employees/{id}', [EmployeeController::class, 'destroy']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
